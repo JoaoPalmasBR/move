1108257ca4ed71db140f0e8a6c78a6450ccf67a2
Joao Antonio <joaoantonios74@gmail.com>
2025-04-15 04:39:14 -0300

    feat: update success message to include email validation after account creation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -39,7 +39,7 @@ export default function LoginScreen() {
       } else {
         Toast.show({
           type: 'success',
-          text1: 'Conta criada com sucesso! Faça login.',
+          text1: 'Conta criada com sucesso! Valide seu email e faça login.',
         });
         //Alert.alert('Sucesso', 'Conta criada com sucesso! Faça login.');
         setIsRegistering(false);
