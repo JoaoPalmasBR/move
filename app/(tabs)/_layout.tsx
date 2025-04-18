975a52994d083b02a8faad868e17a73024eaed29
Joao Antonio <joaoantonios74@gmail.com>
2025-04-18 01:36:29 -0300

    add base da tela de locais

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -37,6 +37,15 @@ export default function TabsLayout() {
         }}
       />
       <Tabs.Screen
+        name="locais"
+        options={{
+          title: 'Locais',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="location-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="perfil"
         options={{
           title: 'Perfil',
